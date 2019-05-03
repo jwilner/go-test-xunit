dfe25f8fd49a8f076862a4ee78b35fa210e3f7a2
Joe Wilner <jhwilner1@gmail.com>
2019-05-02 23:00:27 -0400

    travisssss

--- .travis/script.sh
+++ .travis/script.sh
@@ -5,7 +5,7 @@ set -e
 function main {
     go test ./...
 
-    mkdir target
+    mkdir -p target
     GOOS=darwin GOARCH=amd64 CGO_ENABLED=0 go build -o target/go-test-xunit-darwin-amd64
     GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o target/go-test-xunit-linux-amd64
 }
